5ecd6a4d7c10cedfa486ec975ce3890c2bb05e08
David Hovemeyer <david.hovemeyer@gmail.com>
2017-07-10 20:00:24 -0400

    implemented ISR for polling/display

--- fw/busmon3.c
+++ fw/busmon3.c
@@ -4,6 +4,7 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+#if 0
 int main(void) {
 	// Just a test to light all segments for now
 	DDRA = 0xff;
@@ -18,8 +19,9 @@ int main(void) {
 	for (;;) {
 	}
 }
+#endif
 
-#if 0
+#if 1
 #define A (1<<0)
 #define B (1<<1)
 #define C (1<<2)
@@ -28,7 +30,7 @@ int main(void) {
 #define F (1<<5)
 #define G (1<<6)
 
-const uint8_t hexfont[16] = {
+const uint8_t g_hexfont[16] = {
 	~(A|B|C|D|E|F),
 	~(B|C),
 	~(A|B|D|E|G),
@@ -47,33 +49,55 @@ const uint8_t hexfont[16] = {
 	~(A|E|F|G),
 };
 
+uint8_t g_count;
+
 ISR(TIMER0_OVF_vect) {
-	uint8_t which = PORTA & 0x3;
-
-	uint8_t val = PINB;
-
-	if (which == 2) {
-		// currently displaying left digit, switch to right
-		PORTA = 0xfd;
-		PORTD = hexfont[val & 0xF];
-	} else {
-		// currently displaying right digit, switch to left
-		PORTA = 0xfe;
-		PORTD = hexfont[(val >> 4) & 0xF];
+#if 1
+	uint8_t which = g_count & 0x3;
+	uint8_t val;
+	switch (which) {
+	case 0: // left high
+		val = (PINC >> 4) & 0x0f;
+		PORTB = ~1;
+		PORTA = g_hexfont[val];
+		break;
+	case 1: // left low
+		val = PINC & 0x0f;
+		PORTB = ~2;
+		PORTA = g_hexfont[val];
+		break;
+	case 2: // right high
+		val = (PIND >> 4) & 0x0f;
+		PORTB = ~4;
+		PORTA = g_hexfont[val];
+		break;
+	case 3: // right low
+		val = PIND & 0x0f;
+		PORTB = ~8;
+		PORTA = g_hexfont[val];
+		break;
 	}
+	g_count++;
+#endif
+#if 0
+	PORTA = 0x00;
+	PORTB = 0xf0;
+#endif
 }
 
 int main(void) {
-	DDRA = 0x03; // A0,A1 configured as output, all others configured as input
-	PORTA = 0xfe; // enable pull ups (and write 1 to A1, 0 to A0)
-	DDRB = 0x00; // all pins configured as input
-	PORTB = 0xff; // enable pull ups
-	DDRD = 0xff; // all pins configured as output
-	PORTD = 0xff; // write 1 to all pins
+	DDRA = 0xff;
+	PORTA = 0xff; // all segments off initially
+	DDRB = 0x0f;  // all displays off initially
+	PORTB = 0xf0; // enable pull-ups on unused pins
+	DDRC = 0x00;
+	PORTC = 0xff; // enable pull-ups
+	DDRD = 0x00;
+	PORTD = 0xff; // enable pull-ups
 
 	// Set up timer interrupt to fire 4096 times per second
-	TCCR0B |= (1<<CS00);   // no prescaling
-	TIMSK |= (1 << TOIE0); // interrupt on overflow of counter 0
+	TCCR0B |= (1 << CS00);
+	TIMSK0 |= (1 << TOIE0);
 
 	sei(); // enable interrupts
 
